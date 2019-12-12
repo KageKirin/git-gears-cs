07f44defd338aa612b8f62f65f0e9b333a9c9fb1
KageKirin <kagekirin@gmail.com>
2019-12-12 16:35:45 +0900

    Clang format

--- src/Program.cs
+++ src/Program.cs
@@ -1,12 +1,13 @@
 ﻿using System;
+using LibGit2Sharp;
 
 namespace git_gears
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+class Program
+{
+	static void Main(string[] args)
+	{
+		Console.WriteLine("Hello World!");
+	}
+}
 }
