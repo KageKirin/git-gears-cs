49bb9a90fa7a189e7a1a4d2f7d7fa0236f1b9dd3
KageKirin <kagekirin@gmail.com>
2019-12-12 18:37:40 +0900

    TEST program options parsing

--- src/Program.cs
+++ src/Program.cs
@@ -2,11 +2,48 @@
 using System.IO;
 using System.Linq;
 using LibGit2Sharp;
+using CommandLine;
 
 namespace git_gears
 {
 class Program
 {
+	/// <summary>
+	/// Program options
+	/// </summary>
+	public abstract class CommonOptions
+	{
+		[Option('v', "verbose", Required = false, HelpText = "Set output to verbose messages.")]
+		public bool Verbose
+		{
+			get;
+			set;
+		}
+	}
+
+	[Verb("foobar", HelpText = "foobar the hoge.")]
+	class FoobarOptions : CommonOptions
+	{
+		[Option('h', "hoge", Required = false, HelpText = "hoge to foobar.")]
+		public bool Hoge
+		{
+			get;
+			set;
+		}
+	}
+
+	[Verb("action", HelpText = "actionate the action.")]
+	class ActionOptions : CommonOptions
+	{
+		[Option('a', "aaaa", Required = false, HelpText = "aaaa.")]
+		public bool Aaaa
+		{
+			get;
+			set;
+		}
+	}
+
+
 	/// <summary>
 	/// find the .git repo path for current repo
 	/// </summary>
@@ -27,8 +64,28 @@ class Program
 		return null;
 	}
 
-	static void Main(string[] args)
+	static int RunFoobar(FoobarOptions opts)
 	{
+		Console.WriteLine($"hoge: {opts.Hoge}");
+		return 0;
+	}
+
+	static int RunAction(ActionOptions opts)
+	{
+		Console.WriteLine($"aaaa: {opts.Aaaa}");
+		return 0;
+	}
+
+	static int Main(string[] args)
+	{
+		return Parser.Default
+			.ParseArguments<FoobarOptions, ActionOptions>(args)
+			.MapResult(
+				(FoobarOptions opts) => RunFoobar(opts),
+				(ActionOptions opts) => RunAction(opts),
+				(errs) => 1
+			);
+
 		using(var repo = new Repository(FindCurrentRepoPath()))
 		{
 			Console.WriteLine(repo.Config.HasConfig(ConfigurationLevel.Global));
@@ -42,7 +99,6 @@ class Program
 
 			if (repo.Config.HasConfig(ConfigurationLevel.Local))
 			{
-
 				foreach(var c in repo.Config.Find(@"(gears).*", ConfigurationLevel.Local))
 				{
 					Console.WriteLine("{0} -> {1}", c.Key, c.Value);
@@ -51,7 +107,6 @@ class Program
 
 			if (repo.Config.HasConfig(ConfigurationLevel.Global))
 			{
-
 				foreach(var c in repo.Config.Find(@"(gears).*", ConfigurationLevel.Global))
 				{
 					Console.WriteLine("{0} -> {1}", c.Key, c.Value);
