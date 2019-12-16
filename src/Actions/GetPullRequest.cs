affb6ce6756a118c313fa0816ecc0b14b30ca925
KageKirin <kagekirin@gmail.com>
2019-12-16 14:37:19 +0900

    Add action get-pullrequest + options

---
+++ src/Actions/GetPullRequest.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace git_gears
+{
+public class GetPullRequest
+{
+	public static int Execute(GetPullRequestOptions opts)
+	{
+		Console.WriteLine(System.Reflection.MethodBase.GetCurrentMethod().Name);
+		return 0;
+	}
+}
+}
