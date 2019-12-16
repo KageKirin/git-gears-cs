a5ceaa10b10e156bcb194310963f9c27025fca78
KageKirin <kagekirin@gmail.com>
2019-12-16 14:38:11 +0900

    Add action list-issues + options

---
+++ src/Actions/ListIssues.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace git_gears
+{
+public class ListIssues
+{
+	public static int Execute(ListIssuesOptions opts)
+	{
+		Console.WriteLine(System.Reflection.MethodBase.GetCurrentMethod().Name);
+		return 0;
+	}
+}
+}
