4a95a00273f83cc919e00724418c5ad6c3c463e5
KageKirin <kagekirin@gmail.com>
2019-12-16 14:39:18 +0900

    Plug all Get- and List- actions in main

--- src/Program.cs
+++ src/Program.cs
@@ -1,13 +1,30 @@
 ﻿using System;
 using LibGit2Sharp;
+using CommandLine;
 
 namespace git_gears
 {
 class Program
 {
-	static void Main(string[] args)
+	static int Main(string[] args)
 	{
-		Console.WriteLine("Hello World!");
+		return Parser.Default
+			.ParseArguments<GetGistOptions,			 //
+							GetIssueOptions,		 //
+							GetPullRequestOptions,	 //
+							GetRepoOptions,			 //
+							ListGistsOptions,		 //
+							ListIssuesOptions,		 //
+							ListPullRequestsOptions, //
+							ListReposOptions>(args)
+			.MapResult((GetGistOptions opts) => GetGist.Execute(opts), //
+					   (GetIssueOptions opts) => GetIssue.Execute(opts),
+					   (GetPullRequestOptions opts) => GetPullRequest.Execute(opts),
+					   (GetRepoOptions opts) => GetRepo.Execute(opts),
+					   (ListGistsOptions opts) => ListGists.Execute(opts),
+					   (ListIssuesOptions opts) => ListIssues.Execute(opts),
+					   (ListPullRequestsOptions opts) => ListPullRequests.Execute(opts),
+					   (ListReposOptions opts) => ListRepos.Execute(opts), (errs) => 1);
 	}
 }
 }
