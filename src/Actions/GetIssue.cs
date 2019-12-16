0245b08d1b4866e1da64786ed8b7e5836c0c7001
KageKirin <kagekirin@gmail.com>
2019-12-16 14:36:57 +0900

    Add action get-issue + options

---
+++ src/Actions/GetIssue.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace git_gears
+{
+public class GetIssue
+{
+	public static int Execute(GetIssueOptions opts)
+	{
+		Console.WriteLine(System.Reflection.MethodBase.GetCurrentMethod().Name);
+		return 0;
+	}
+}
+}
