53d4112962af12da03de849f88e7f9fa7d7c03d5
KageKirin <kagekirin@gmail.com>
2019-12-16 14:37:51 +0900

    Add action list-gists + options

---
+++ src/Actions/ListGists.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace git_gears
+{
+public class ListGists
+{
+	public static int Execute(ListGistsOptions opts)
+	{
+		Console.WriteLine(System.Reflection.MethodBase.GetCurrentMethod().Name);
+		return 0;
+	}
+}
+}
---
+++ src/Options/ListGistsOptions.cs
@@ -0,0 +1,9 @@
+using CommandLine;
+
+namespace git_gears
+{
+[Verb("list-gists", HelpText = "List the gists.")]
+public class ListGistsOptions : CommonListOptions
+{
+}
+}
