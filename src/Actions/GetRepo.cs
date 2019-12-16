d31bcdb86c9a868b50838450cd299b121e2dc045
KageKirin <kagekirin@gmail.com>
2019-12-16 14:37:32 +0900

    Add action get-repo + options

---
+++ src/Actions/GetRepo.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace git_gears
+{
+public class GetRepo
+{
+	public static int Execute(GetRepoOptions opts)
+	{
+		Console.WriteLine(System.Reflection.MethodBase.GetCurrentMethod().Name);
+		return 0;
+	}
+}
+}
