b4940b49c584c1bb088f94f37a54ca16b950b90e
KageKirin <kagekirin@gmail.com>
2019-12-16 14:38:27 +0900

    Add action list-pullrequests + options

---
+++ src/Actions/ListPullRequests.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace git_gears
+{
+public class ListPullRequests
+{
+	public static int Execute(ListPullRequestsOptions opts)
+	{
+		Console.WriteLine(System.Reflection.MethodBase.GetCurrentMethod().Name);
+		return 0;
+	}
+}
+}
