d6adb2e1df986a121cf9d4747c9ed3ce2a62a8b2
KageKirin <kagekirin@gmail.com>
2019-12-16 14:35:26 +0900

    Add Common Options

---
+++ src/Options/CommonOptions.cs
@@ -0,0 +1,30 @@
+using CommandLine;
+
+namespace git_gears
+{
+public abstract class CommonOptions
+{
+	[Option('v', "verbose", Required = false, HelpText = "Verbose printing of internal actions.")]
+	public bool Verbose
+	{
+		get;
+		set;
+	}
+
+	[Value(0, MetaName = "remote", HelpText = "Remote.", Default="origin")]
+	public string Remote
+	{
+		get;
+		set;
+	}
+
+	[Value(1, MetaName = "branch", HelpText = "Branch.", Default="current")] //TODO: Default="current branch"
+	public string Branch
+	{
+		get;
+		set;
+	}
+
+	
+}
+}
