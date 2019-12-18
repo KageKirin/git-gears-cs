6dab59e84bcfcb89c4912c2fd099a6f9d6da16af
KageKirin <kagekirin@gmail.com>
2019-12-18 17:42:50 +0900

    Implement GitLabGear.Test()

--- src/Gears/GitLabGear.cs
+++ src/Gears/GitLabGear.cs
@@ -1,3 +1,9 @@
+using System;
+using System.IO;
+using System.Linq;
+using GraphQL.Client;
+using GraphQL.Common.Request;
+using GraphQL.Common.Response;
 
 namespace git_gears
 {
@@ -5,7 +11,33 @@ public class GitLabGear : CommonGear, IGear
 {
 	public GitLabGear(string remote) : base(remote)
 	{
+	}
+
+	public void Test()
+	{
+		var graphQLHttpRequest = new GraphQLRequest {
+			Query = @"
+				query ($_fullPath: ID!) {
+					project(fullPath: $_fullPath)
+					{
+						id,
+						name,
+						webUrl
+					}
+				}",
+			Variables = new {
+				_fullPath = $"{RepoUrl.Owner}/{RepoUrl.RepoName}"
+			}
+		};
+		Console.WriteLine($"{graphQLHttpRequest.Query}");
+		Console.WriteLine($"{graphQLHttpRequest.Variables}");
+		GraphQLResponse graphQLHttpResponse = Client.PostAsync(graphQLHttpRequest).Result;
+		Console.WriteLine($"{graphQLHttpResponse.Data != null}");
 		
+		if (graphQLHttpResponse.Data != null)
+		{
+			Console.WriteLine($"{graphQLHttpResponse.Data.ToString()}");
+		}
 	}
 }
 }
