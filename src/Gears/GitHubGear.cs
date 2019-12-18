bcdcfff879e4f90a02b1a6997561e35df5d02b26
KageKirin <kagekirin@gmail.com>
2019-12-18 17:43:05 +0900

    Implement GitHubGear.Test()

--- src/Gears/GitHubGear.cs
+++ src/Gears/GitHubGear.cs
@@ -1,9 +1,9 @@
 using System;
 using System.IO;
 using System.Linq;
-using GraphQL;
-using GraphQL.Types;
-
+using GraphQL.Client;
+using GraphQL.Common.Request;
+using GraphQL.Common.Response;
 
 namespace git_gears
 {
@@ -11,12 +11,34 @@ public class GitHubGear : CommonGear, IGear
 {
 	public GitHubGear(string remote) : base(remote)
 	{
-
 	}
 
 	public void Test()
 	{
-
+		var graphQLHttpRequest = new GraphQLRequest {
+			Query = @"
+				query($_owner: String!, $_name: String!){
+					repository(owner: $_owner, name: $_name)
+					{
+						id,
+						name,
+						url
+					}
+				}",
+			Variables = new {
+				_owner = RepoUrl.Owner,
+				_name  = RepoUrl.RepoName,
+			}
+		};
+		Console.WriteLine($"{graphQLHttpRequest.Query}");
+		Console.WriteLine($"{graphQLHttpRequest.Variables}");
+		GraphQLResponse graphQLHttpResponse = Client.PostAsync(graphQLHttpRequest).Result;
+		Console.WriteLine($"{graphQLHttpResponse.Data != null}");
+		
+		if (graphQLHttpResponse.Data != null)
+		{
+			Console.WriteLine($"{graphQLHttpResponse.Data.ToString()}");
+		}
 	}
 }
 }
