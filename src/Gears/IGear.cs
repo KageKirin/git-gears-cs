8de95199f05dcbd26ea6dc23345f611bdd6cc43e
KageKirin <kagekirin@gmail.com>
2019-12-19 15:38:27 +0900

    Add IGear interface functions
    GetGist()
    GetIssue()
    GetPullRequest()
    GetRepo()
    
    ListGists()
    ListIssues()
    ListPullRequests()
    ListRepos()

--- src/Gears/IGear.cs
+++ src/Gears/IGear.cs
@@ -1,8 +1,19 @@
+using System.Collections.Generic;
 
 namespace git_gears
 {
 public interface IGear
 {
 	void Test();
+
+	Gist GetGist();
+	Issue GetIssue();
+	PullRequest GetPullRequest();
+	Repo GetRepo();
+
+	IEnumerable<Gist> ListGists();
+	IEnumerable<Issue> ListIssues();
+	IEnumerable<PullRequest> ListPullRequests();
+	IEnumerable<Repo> ListRepos();
 }
 }
