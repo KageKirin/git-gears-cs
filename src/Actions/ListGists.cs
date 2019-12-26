54010926bdfd50436b322ace8a23a1dcfdf17024
KageKirin <kagekirin@gmail.com>
2019-12-26 14:21:11 +0900

    Implement 'ListGists' action

--- src/Actions/ListGists.cs
+++ src/Actions/ListGists.cs
@@ -7,6 +7,19 @@ public class ListGists
 	public static int Execute(ListGistsOptions opts)
 	{
 		Console.WriteLine(System.Reflection.MethodBase.GetCurrentMethod().Name);
+
+		var gear = GearFactory.CreateGear(opts.Remote);
+		Console.WriteLine($"gear: {gear.ToString()}");
+		if (gear != null)
+		{
+			Console.WriteLine("-- ListGists --");
+			var gists = gear.ListGists();
+			foreach(var i in gists)
+			{
+				Console.WriteLine($"{i.Description} - {i.Name} - {i.Id}");
+			}
+		}
+
 		return 0;
 	}
 }
