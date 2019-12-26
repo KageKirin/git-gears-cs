1bc9106f1a81d2dbfd891a746ca44a015bbd4a0f
KageKirin <kagekirin@gmail.com>
2019-12-26 12:11:47 +0900

    Add TODOs to IGear

--- src/Gears/IGear.cs
+++ src/Gears/IGear.cs
@@ -6,14 +6,14 @@ public interface IGear
 {
 	void Test();
 
-	Gist GetGist();
-	Issue GetIssue();
-	PullRequest GetPullRequest();
-	Repo GetRepo();
+	Gist GetGist();				  //! TODO: params
+	Issue GetIssue();			  //! TODO: params
+	PullRequest GetPullRequest(); //! TODO: params
+	Repo GetRepo();				  //! TODO: params
 
-	IEnumerable<Gist> ListGists();
-	IEnumerable<Issue> ListIssues();
-	IEnumerable<PullRequest> ListPullRequests();
-	IEnumerable<Repo> ListRepos();
+	IEnumerable<Gist>ListGists();				//! TODO: params + filter params
+	IEnumerable<Issue>ListIssues();				//! TODO: params + filter params
+	IEnumerable<PullRequest>ListPullRequests(); //! TODO: params + filter params
+	IEnumerable<Repo>ListRepos();				//! TODO: params + filter params
 }
 }
