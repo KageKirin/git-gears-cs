69889079b58c763613e6abc0da660360cd556835
KageKirin <kagekirin@gmail.com>
2019-12-26 12:19:11 +0900

    Implement GetRepo action

--- src/Actions/GetRepo.cs
+++ src/Actions/GetRepo.cs
@@ -7,6 +7,25 @@ public class GetRepo
 	public static int Execute(GetRepoOptions opts)
 	{
 		Console.WriteLine(System.Reflection.MethodBase.GetCurrentMethod().Name);
+
+		var gear = GearFactory.CreateGear(opts.Remote);
+		Console.WriteLine($"gear: {gear.ToString()}");
+
+		if (gear != null)
+		{
+			var repo = gear.GetRepo();
+			if (repo != null)
+			{
+				Console.WriteLine($"Repo for {opts.Remote}");
+				Console.WriteLine($"{repo.Name} -- {repo.Description}");
+				Console.WriteLine($"{repo.Url}");
+			}
+			else
+			{
+				Console.WriteLine($"There is no repository to be found for {opts.Remote}");
+			}
+		}
+
 		return 0;
 	}
 }
