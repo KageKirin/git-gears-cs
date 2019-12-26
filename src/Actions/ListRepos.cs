63b5b1ea22f8e125e2caf090ac5948412b717a1e
KageKirin <kagekirin@gmail.com>
2019-12-26 14:40:11 +0900

    Implement ListRepos action

--- src/Actions/ListRepos.cs
+++ src/Actions/ListRepos.cs
@@ -7,6 +7,19 @@ public class ListRepos
 	public static int Execute(ListReposOptions opts)
 	{
 		Console.WriteLine(System.Reflection.MethodBase.GetCurrentMethod().Name);
+
+		var gear = GearFactory.CreateGear(opts.Remote);
+		Console.WriteLine($"gear: {gear.ToString()}");
+		if (gear != null)
+		{
+			Console.WriteLine("-- ListRepos --");
+			var repos = gear.ListRepos();
+			foreach(var i in repos)
+			{
+				Console.WriteLine($"{i.Name} - {i.Description} - {i.Url}");
+			}
+		}
+
 		return 0;
 	}
 }
