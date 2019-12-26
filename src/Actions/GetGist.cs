7330286a01841183b0580338d768e4e06a4ac74f
KageKirin <kagekirin@gmail.com>
2019-12-26 14:56:50 +0900

    Implement 'GetGist' action

--- src/Actions/GetGist.cs
+++ src/Actions/GetGist.cs
@@ -7,6 +7,23 @@ public class GetGist
 	public static int Execute(GetGistOptions opts)
 	{
 		Console.WriteLine(System.Reflection.MethodBase.GetCurrentMethod().Name);
+
+		var gear = GearFactory.CreateGear(opts.Remote);
+		Console.WriteLine($"gear: {gear.ToString()}");
+
+		if (gear != null)
+		{
+			GistInfo? gist = gear.GetGist(opts.Name);
+			if (gist.HasValue)
+			{
+				Console.WriteLine($"Gist {opts.Name} for {opts.Remote}");
+				Console.WriteLine($"{gist.Value.Description} - {gist.Value.Name} - {gist.Value.Id}");
+			}
+			else
+			{
+				Console.WriteLine($"There is no gist to be found for {opts.Remote} {opts.Name}");
+			}
+		}
 		return 0;
 	}
 }
