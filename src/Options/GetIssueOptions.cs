e0d6524c5acd9064709dad10729cfa956b2f6755
KageKirin <kagekirin@gmail.com>
2019-12-26 13:42:22 +0900

    Add option field 'number' for GetIssue

--- src/Options/GetIssueOptions.cs
+++ src/Options/GetIssueOptions.cs
@@ -5,5 +5,11 @@ namespace git_gears
 [Verb("get-issue", HelpText = "Get the indicated issue.")]
 public class GetIssueOptions : CommonGetOptions
 {
+	[Option("number", Required = true, HelpText = "issue number.")]
+	public int Number
+	{
+		get;
+		set;
+	}
 }
 }
