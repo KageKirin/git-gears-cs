47d9fce357fa81ce27bc28e5771c13b7b6ca7094
KageKirin <kagekirin@gmail.com>
2019-12-26 12:18:41 +0900

    Implement ListPullRequests action

--- src/Actions/ListPullRequests.cs
+++ src/Actions/ListPullRequests.cs
@@ -7,6 +7,19 @@ public class ListPullRequests
 	public static int Execute(ListPullRequestsOptions opts)
 	{
 		Console.WriteLine(System.Reflection.MethodBase.GetCurrentMethod().Name);
+
+		var gear = GearFactory.CreateGear(opts.Remote);
+		Console.WriteLine($"gear: {gear.ToString()}");
+		if (gear != null)
+		{
+			Console.WriteLine("-- ListPullRequests --");
+			var prs = gear.ListPullRequests();
+			foreach(var i in prs)
+			{
+				Console.WriteLine($"#{i.Number} - {i.Title} - {i.Url}");
+			}
+		}
+
 		return 0;
 	}
 }
