d013067dbc75369bdd40064309e659a6f64b26bc
KageKirin <kagekirin@gmail.com>
2020-01-08 16:47:15 +0900

    Add CreateIssue action

---
+++ src/Actions/CreateIssue.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace git_gears
+{
+public class CreateIssue
+{
+	public static int Execute(CreateIssueOptions opts)
+	{
+		Console.WriteLine(System.Reflection.MethodBase.GetCurrentMethod().Name);
+
+		var gear = GearFactory.CreateGear(opts.Remote);
+		Console.WriteLine($"gear: {gear.ToString()}");
+
+		if (gear != null)
+		{
+			IssueInfo? issue = gear.CreateIssue(opts.Title, opts.Body);
+			if (issue.HasValue)
+			{
+				Console.WriteLine($"Created new issue on {opts.Remote}");
+				Console.WriteLine($"#{issue.Value.Number} - {issue.Value.Title} - {issue.Value.Url}");
+				Console.WriteLine($"{issue.Value.Body}");
+			}
+			else
+			{
+				Console.WriteLine($"Failed to create an issue on {opts.Remote}");
+			}
+		}
+		return 0;
+	}
+}
+}
