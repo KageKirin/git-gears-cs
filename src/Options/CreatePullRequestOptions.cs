0d6cfaa5a1d06dfb6a139f85f3e05a83bf0373d8
KageKirin <kagekirin@gmail.com>
2020-01-09 11:39:25 +0900

    Add options for CreatePullRequest

---
+++ src/Options/CreatePullRequestOptions.cs
@@ -0,0 +1,38 @@
+using CommandLine;
+
+namespace git_gears
+{
+[Verb(
+	"create-pullrequest",
+	HelpText =
+		@"Create a new pullrequest (aka mergerequest)
+		from the given remote and branch into a target (remote) branch,
+		respectively target repo:branch.
+		Please note that this operation will fail if you haven't pushed the branch to the remote yet.")]
+public class CreatePullRequestOptions : CommonCreateOptions
+{
+	[Option('t', "title", Required = false, HelpText = "pullrequest title.", Default = "new pullrequest")]
+	public string Title
+	{
+		get;
+		set;
+	}
+
+	[Option('m', "message", Required = false, HelpText = "pullrequest body in Markdown.",
+			Default = "edit this message")]
+	public string Body
+	{
+		get;
+		set;
+	}
+
+	[Option("into", Required = true,
+			HelpText = @"target 'branch' or 'repo:branch' to merge into.
+			`repo` must be a known remote")]
+	public string TargetBranch
+	{
+		get;
+		set;
+	}
+}
+}
