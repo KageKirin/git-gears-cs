b62ab2fa67d49d136161e5ff8a022673bddeac0e
KageKirin <kagekirin@gmail.com>
2020-01-09 14:25:44 +0900

    Improve default pull request title/body

--- src/Actions/CreatePullRequest.cs
+++ src/Actions/CreatePullRequest.cs
@@ -13,14 +13,17 @@ public class CreatePullRequest
 
 		var branch = opts.Branch ?? GitUtils.GetCurrentBranch();
 		// TODO: parse opts.TargetBranch to extract repo from pattern repo:branch
+		var title = opts.Title ?? $"merge {branch} -> {opts.TargetBranch}";
+		var body = opts.Body ?? $"This PR merges `{branch}` into `{opts.TargetBranch}`.";
+		// TODO: support for opts `--draft`
 
 		if (gear != null)
 		{
 			PullRequestInfo? pr = gear.CreatePullRequest(new CreatePullRequestParams{
-				branch = branch, targetBranch = opts.TargetBranch, title = opts.Title, body = opts.Body});
+				branch = branch, targetBranch = opts.TargetBranch, title = title, body = body});
 			if (pr.HasValue)
 			{
-				Console.WriteLine($"Created new issue on {opts.Remote}");
+				Console.WriteLine($"Created new pull request on {opts.Remote}");
 				Console.WriteLine($"#{pr.Value.Number} - {pr.Value.Title} - {pr.Value.Url}");
 				Console.WriteLine($"{pr.Value.Body}");
 			}
