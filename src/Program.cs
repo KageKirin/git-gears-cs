c269b5511323c0ad61380853775cbc24babe77ab
KageKirin <kagekirin@gmail.com>
2020-01-09 11:39:44 +0900

    Plug CreatePullRequest options/actions to program flow

--- src/Program.cs
+++ src/Program.cs
@@ -9,29 +9,31 @@ class Program
 	static int Main(string[] args)
 	{
 		return Parser.Default
-			.ParseArguments<			 //
-				CreateIssueOptions,		 //
-				GetGistOptions,			 //
-				GetIssueOptions,		 //
-				GetPullRequestOptions,	 //
-				GetRepoOptions,			 //
-				ListGistsOptions,		 //
-				ListIssuesOptions,		 //
-				ListPullRequestsOptions, //
-				ListReposOptions,		 //
-				TestOptions				 //
+			.ParseArguments<			  //
+				CreateIssueOptions,		  //
+				CreatePullRequestOptions, //
+				GetGistOptions,			  //
+				GetIssueOptions,		  //
+				GetPullRequestOptions,	  //
+				GetRepoOptions,			  //
+				ListGistsOptions,		  //
+				ListIssuesOptions,		  //
+				ListPullRequestsOptions,  //
+				ListReposOptions,		  //
+				TestOptions				  //
 				>(args)
-			.MapResult(															  //
-				(CreateIssueOptions opts) => CreateIssue.Execute(opts),			  //
-				(GetGistOptions opts) => GetGist.Execute(opts),					  //
-				(GetIssueOptions opts) => GetIssue.Execute(opts),				  //
-				(GetPullRequestOptions opts) => GetPullRequest.Execute(opts),	  //
-				(GetRepoOptions opts) => GetRepo.Execute(opts),					  //
-				(ListGistsOptions opts) => ListGists.Execute(opts),				  //
-				(ListIssuesOptions opts) => ListIssues.Execute(opts),			  //
-				(ListPullRequestsOptions opts) => ListPullRequests.Execute(opts), //
-				(TestOptions opts) => Test.Execute(opts),						  //
-				(ListReposOptions opts) => ListRepos.Execute(opts),				  //
+			.MapResult(																//
+				(CreateIssueOptions opts) => CreateIssue.Execute(opts),				//
+				(CreatePullRequestOptions opts) => CreatePullRequest.Execute(opts), //
+				(GetGistOptions opts) => GetGist.Execute(opts),						//
+				(GetIssueOptions opts) => GetIssue.Execute(opts),					//
+				(GetPullRequestOptions opts) => GetPullRequest.Execute(opts),		//
+				(GetRepoOptions opts) => GetRepo.Execute(opts),						//
+				(ListGistsOptions opts) => ListGists.Execute(opts),					//
+				(ListIssuesOptions opts) => ListIssues.Execute(opts),				//
+				(ListPullRequestsOptions opts) => ListPullRequests.Execute(opts),	//
+				(TestOptions opts) => Test.Execute(opts),							//
+				(ListReposOptions opts) => ListRepos.Execute(opts),					//
 				(errs) => 1);
 	}
 }
