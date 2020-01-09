6d9006edd9fe8d6834c45ad366d6e99d085c0213
KageKirin <kagekirin@gmail.com>
2020-01-09 16:21:25 +0900

    Add action 'CreateRepo'

---
+++ src/Actions/CreateRepo.cs
@@ -0,0 +1,35 @@
+using System;
+
+namespace git_gears
+{
+public class CreateRepo
+{
+	public static int Execute(CreateRepoOptions opts)
+	{
+		Console.WriteLine(System.Reflection.MethodBase.GetCurrentMethod().Name);
+
+		var gear = GearFactory.CreateGear(opts.Remote);
+		Console.WriteLine($"gear: {gear.ToString()}");
+
+		if (gear != null)
+		{
+			RepoInfo? repo = gear.CreateRepo(new CreateRepoParams{
+				description = opts.Description,
+				homepage = opts.Homepage,
+				isPublic = !opts.IsPrivate,
+			});
+			if (repo.HasValue)
+			{
+				Console.WriteLine($"Created new repo on {opts.Remote}");
+				Console.WriteLine($"{repo.Value.Name} -- {repo.Value.Description} -- {repo.Value.Homepage}");
+				Console.WriteLine($"{repo.Value.Url}");
+			}
+			else
+			{
+				Console.WriteLine($"Failed to create an repo on {opts.Remote}.");
+			}
+		}
+		return 0;
+	}
+}
+}
