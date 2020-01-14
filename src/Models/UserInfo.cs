111d76b00ffad2ceae50bfffc441963ad06295f2
KageKirin <kagekirin@gmail.com>
2020-01-14 19:29:14 +0900

    Add UserInfo model

---
+++ src/Models/UserInfo.cs
@@ -0,0 +1,15 @@
+
+namespace git_gears
+{
+public struct UserInfo
+{
+	public string Id;
+	public string Name;
+	public string Login;
+	public string Email;
+	public string Url;
+	public string Bio;
+	public string Company;
+	public string Website;
+}
+}
