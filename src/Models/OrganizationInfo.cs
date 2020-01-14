9d6f98ed1be266daa4f496c9af6b3a9ebc8f0875
KageKirin <kagekirin@gmail.com>
2020-01-14 19:29:35 +0900

    Add OrganizationInfo model

---
+++ src/Models/OrganizationInfo.cs
@@ -0,0 +1,14 @@
+
+namespace git_gears
+{
+public struct OrganizationInfo
+{
+	public string Id;
+	public string Name;
+	public string Login;
+	public string Email;
+	public string Url;
+	public string Description;
+	public string Website;
+}
+}
