b5303f12a9b96b04687fb57b5eac8ca8410668ef
KageKirin <kagekirin@gmail.com>
2020-01-15 13:33:46 +0900

    Add action 'GetOwner'

---
+++ src/Actions/GetOwner.cs
@@ -0,0 +1,34 @@
+using System;
+
+namespace git_gears
+{
+public class GetOwner
+{
+	public static int Execute(GetOwnerOptions opts)
+	{
+		Console.WriteLine(System.Reflection.MethodBase.GetCurrentMethod().Name);
+
+		var gear = GearFactory.CreateGear(opts.Remote);
+		Console.WriteLine($"gear: {gear.ToString()}");
+
+		var url = GitUtils.GetRemoteUrl(opts.Remote);
+
+		if (gear != null)
+		{
+			OwnerInfo? owner = gear.GetOwner(url.Owner);
+			if (owner.HasValue)
+			{
+				Console.WriteLine($"Owner for {opts.Remote}");
+				Console.WriteLine($"{owner.Value.Name} -- {owner.Value.Login} -- {owner.Value.Url}");
+			}
+			else
+			{
+				Console.WriteLine($"There is no owner to be found for {opts.Remote}.");
+				Console.WriteLine("This is an unexpected result.");
+			}
+		}
+
+		return 0;
+	}
+}
+}
