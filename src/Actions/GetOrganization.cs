7a9f376d4bd62363051579ff20f49fcd8e3aa2f8
KageKirin <kagekirin@gmail.com>
2020-01-15 13:33:30 +0900

    Add action 'GetOrganization'

---
+++ src/Actions/GetOrganization.cs
@@ -0,0 +1,34 @@
+using System;
+
+namespace git_gears
+{
+public class GetOrganization
+{
+	public static int Execute(GetOrganizationOptions opts)
+	{
+		Console.WriteLine(System.Reflection.MethodBase.GetCurrentMethod().Name);
+
+		var gear = GearFactory.CreateGear(opts.Remote);
+		Console.WriteLine($"gear: {gear.ToString()}");
+
+		var url = GitUtils.GetRemoteUrl(opts.Remote);
+
+		if (gear != null)
+		{
+			OrganizationInfo? org = gear.GetOrganization(url.Owner);
+			if (org.HasValue)
+			{
+				Console.WriteLine($"Owning organization for {opts.Remote}");
+				Console.WriteLine($"{org.Value.Name} -- {org.Value.Description} -- {org.Value.Url} -- {org.Value.Website}");
+			}
+			else
+			{
+				Console.WriteLine($"There is no owning organization to be found for {opts.Remote}.");
+				Console.WriteLine("Try `get-user` or `get-owner`.");
+			}
+		}
+
+		return 0;
+	}
+}
+}
