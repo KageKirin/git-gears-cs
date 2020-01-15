98655feaadb8cd2e6043432372e548fc69710dd6
KageKirin <kagekirin@gmail.com>
2020-01-15 13:32:01 +0900

    Add options for GetOrganization

---
+++ src/Options/GetOrganizationOptions.cs
@@ -0,0 +1,9 @@
+using CommandLine;
+
+namespace git_gears
+{
+[Verb("get-organization", HelpText = "Get the repo owning organization.")]
+public class GetOrganizationOptions : CommonGetOptions
+{
+}
+}
